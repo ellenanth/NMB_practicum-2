6b0fbad825ec876efd9b4ed5a84afccf1a876873
MarteBiesmans <n-abbe@hotmail.com>
2017-05-15 22:04:19 +0200

    kkb opgeruimd

--- MATLAB/kkb.m
+++ MATLAB/kkb.m
@@ -1,19 +1,13 @@
 function C = kkb(x,y,F,m,n)
-[M,~] = size(x);
-[N,~] = size(y);
-
 %construct A and B
-A = zeros(M,m+1);
-B = zeros(N,n+1);
+A = zeros(size(x,1),m+1);
+B = zeros(size(y,1),n+1);
 for k = 0:m
-    A(1:M, k+1) = x.^k;
+    A(:,k+1) = x.^k;
 end
 for l = 0:n
     B(:,l+1) = y.^l;
 end
-
 %calculate C
-temp1 = B\F;
-temp2 = pinv(A)';
-C = temp1 * temp2;
+C = B\F * pinv(A)';
 end
