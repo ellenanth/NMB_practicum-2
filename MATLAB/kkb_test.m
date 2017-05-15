cf955e961aaa8cc757f4408531820b65db547e41
MarteBiesmans <n-abbe@hotmail.com>
2017-05-15 22:43:34 +0200

    kkb test opgeruimd

--- MATLAB/kkb_test.m
+++ MATLAB/kkb_test.m
@@ -1,19 +1,31 @@
-x = [1 2 3]';
-y = [1 2 3]';
-F = [1 2 1;2 3 2;1 2 1];
+clear;
+x_i = [1;2;3];
+y_i = [1;2;3];
+F_i= [1 2 1;2 3 2;1 2 1];
 m = 3;
-n = 3;
-C = kkb(x,y,F,m,n);
-surf(x,y,F);
-%TODO titel
+n = 2;
+C = kkb(x_i,y_i,F_i,m,n);
 
-F_new = zeros(3,3);
-x = (1:0.1:3)';
-y = (1:0.1:3)';
-for i = 1:size(x,1)
-    for j = 1:size(y,1)
-        F_new(i,j) = kkb_evaluate(C,x(i),y(j));
+x_s = [];
+y_s = [];
+F_s = [];
+for i=1:size(x_i,1)
+    for j=1:size(y_i,1)
+        x_s = [x_s; x_i(i)];
+        y_s = [y_s; y_i(j)];
+        F_s = [F_s; F_i(i,j)];
     end
 end
-surf(x,y,F_new);
+
+x_o = linspace(1,3,30);
+y_o = linspace(1,3,30);
+F_o = polyval2(C,x_o,y_o);
+
+surf(x_o,y_o,F_o);
+hold on
+scatter3(x_s,y_s,F_s, 50, 'filled','r');
+xlabel x
+ylabel y
+zlabel z
+set(get(gca,'ZLabel'),'Rotation',0)
 %TODO titel
