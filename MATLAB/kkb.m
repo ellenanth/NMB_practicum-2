b3453d075c35a3c854943b76f5192cd308a4068e
MarteBiesmans <n-abbe@hotmail.com>
2017-05-16 13:36:23 +0200

    mergen op een vreemde manier

--- MATLAB/kkb.m
+++ MATLAB/kkb.m
@@ -1,4 +1,13 @@
 function C = kkb(x,y,F,m,n)
+% Benader een oppervlak op basis van de functiewaarden F op een rechthoekig
+% puntenrooster (x_i, y_i) met als benaderende functie een bivariate
+% veelterm van graad m in x en graad n in y.
+% x: vector met M waarden, interpolatiepunten op x-as
+% y: vector met N waarden
+% F: matrix met functiewaarden
+% m: graad in x van benaderende functie
+% n: graad in y van benaderende functie
+
 %construct A and B
 A = zeros(size(x,2),m+1);
 B = zeros(size(y,2),n+1);
